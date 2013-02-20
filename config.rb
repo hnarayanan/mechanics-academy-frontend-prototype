24c007dbf9635b7b5758d13a2392142b793b36d7
Harish Narayanan <hnarayanan@gmail.com>
2013-02-20 17:26:38 +0100

    Clean up randomly generated course names and descriptions to fit within the template

--- config.rb
+++ config.rb
@@ -3,11 +3,13 @@ set :js_dir, 'js'
 set :images_dir, 'img'
 
 helpers do
-  def course_name(length=2)
-    lorem.words(length).split.map {|word| word.capitalize }.join(" ")
+  def course_name
+    length = Random.rand(0..1) + 2
+    lorem.words(length).slice(0, 18).split.map {|word| word.capitalize }.join(" ")
   end
-  def course_description(length=8)
-    lorem.words(length).capitalize + '.'
+  def course_description
+    length = Random.rand(0..4) + 8
+    lorem.words(length).slice(0, 60).capitalize + '.'
   end
 end
 
